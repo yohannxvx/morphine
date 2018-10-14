db2fea1c37210defb8e45ed4e509de38f1471ef5
yohannxvx <44141524+yohannxvx@users.noreply.github.com>
2018-10-15 01:45:13 +0800

    Update viral.sh

--- viral.sh
+++ viral.sh
@@ -14,12 +14,14 @@ echo  " "
 echo  "  Installation will begin in a few moments.."
 sleep 5 
 
+cd
+apt-get update -y && apt-get install -f -y && apt-get install -y tmux && apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make  && cd && apt-get install -y git build-essential cmake libuv1-dev nvidia-cuda-dev nvidia-cuda-toolkit libmicrohttpd-dev && git clone https://github.com/xmrig/xmrig-nvidia.git && cd xmrig-nvidia && mkdir build && cd build && cmake .. -DCUDA_ARCH="20;30;50" && make
+
 clear
  setterm -foreground white
 clear
 
-cd
-apt-get update -y && apt-get install -f -y && apt-get install -y tmux && apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make  && cd && apt-get install -y git build-essential cmake libuv1-dev nvidia-cuda-dev nvidia-cuda-toolkit libmicrohttpd-dev && git clone https://github.com/xmrig/xmrig-nvidia.git && cd xmrig-nvidia && mkdir build && cd build && cmake .. -DCUDA_ARCH="20;30;50" && make
-
 rm -r viral.sh
+
+
 cd
