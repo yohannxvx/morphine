16c1195bc1e581ce0bc51eaeedd26cbfd330b221
yohannxvx <44141524+yohannxvx@users.noreply.github.com>
2018-10-15 01:42:29 +0800

    Update viral.sh

--- viral.sh
+++ viral.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+#XMRig CPU and GPU miner
+#Created by yohannxvx
+#Tested on with Ubuntu 16.04
 
-#Ubuntu
+clear
+ setterm -foreground green
+clear
+
+echo  "  XMRig CPU and GPU installer for Ubuntu 16.04"
+echo  " "
+echo  "  Created by yohannxvx"
+echo  " "
+echo  "  Installation will begin in a few moments.."
+sleep 5 
+
+clear
+ setterm -foreground white
+clear
+
 cd
 apt-get update -y && apt-get install -f -y && apt-get install -y tmux && apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make  && cd && apt-get install -y git build-essential cmake libuv1-dev nvidia-cuda-dev nvidia-cuda-toolkit libmicrohttpd-dev && git clone https://github.com/xmrig/xmrig-nvidia.git && cd xmrig-nvidia && mkdir build && cd build && cmake .. -DCUDA_ARCH="20;30;50" && make
 
