4bb6bae4beb99002c70fdd40c79ad05a5d7ea25c
yohannxvx <44141524+yohannxvx@users.noreply.github.com>
2018-10-15 02:09:22 +0800

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -3,10 +3,6 @@
 #Created by yohannxvx
 #Tested on with Ubuntu 16.04 and Debian 8.x
 
-clear
- setterm -foreground green
-clear
-
 echo  "  XMRig CPU installer for Ubuntu 16.04 and Debian 8.x"
 echo  " "
 echo  "  Created by yohannxvx"
@@ -16,12 +12,5 @@ sleep 5
 
 cd
 apt-get update -y && apt-get install -f -y && apt-get install -y tmux && apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make 
-
-clear
- setterm -foreground white
-clear
-
-rm -r cpu.sh
-
 
 cd
