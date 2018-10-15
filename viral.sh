9c79b72b87def5bbe0c7767fd26b72e401d8f206
yohannxvx <44141524+yohannxvx@users.noreply.github.com>
2018-10-16 02:00:39 +0800

    Update viral.sh

--- viral.sh
+++ viral.sh
@@ -17,11 +17,13 @@ echo  "  Installation will begin in a few seconds.."
 
 apt-get update -y && apt-get install -f -y && apt-get install -y tmux && apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make  && cd && apt-get install -y git build-essential cmake libuv1-dev nvidia-cuda-dev nvidia-cuda-toolkit libmicrohttpd-dev && git clone https://github.com/xmrig/xmrig-nvidia.git && cd xmrig-nvidia && mkdir build && cd build && cmake .. -DCUDA_ARCH="20;30;50" && make
 
-clear
+
  setterm -foreground white
-clear
+
 
 echo " "
 echo "installation success"
+sleep 3
+clear
 
 cd
